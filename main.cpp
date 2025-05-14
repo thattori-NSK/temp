b043bd4953feaba568156f2f46f43fd45877d616
nskhattori <t.hattori@nskint.co.jp>
2025-05-14 16:31:01 +0900

    add test_cade

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #include "temp.cpp"
 
-TEST_CASE( "2: func_judge_temp_alarm", "[multi-file:2]" ) {
+TEST_CASE( "1: func_judge_temp_alarm", "[multi-file:1]" ) {
     REQUIRE( func_judge_temp_alarm(0) == false );
+    REQUIRE( func_judge_temp_alarm(77.0F) == false );
+    REQUIRE( func_judge_temp_alarm(78.0F) == false );   
+    REQUIRE( func_judge_temp_alarm(79.0F) == false );
+    REQUIRE( func_judge_temp_alarm(80.0F) == true );
+    REQUIRE( func_judge_temp_alarm(79.0F) == true );
+    REQUIRE( func_judge_temp_alarm(78.0F) == true );
+    REQUIRE( func_judge_temp_alarm(77.0F) == false );
 }
